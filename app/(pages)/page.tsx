7f2934363d90e6fa0ad2a8a07d99498443295b27
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2024-09-09 15:27:11 -0400

    chapter 14

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -7,7 +7,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
