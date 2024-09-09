780d3b929d832a46361cbbdd0d307dc147f352c4
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2024-09-09 17:23:12 -0400

    chapter 15

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -8,6 +8,8 @@ import {
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
+import { auth } from "@/auth";
+import { UserIcon } from "@heroicons/react/24/outline";
 
 const CardSectionSkeleton = () => {
   return Array.from({ length: 4 }, (_, index) => {
@@ -16,11 +18,21 @@ const CardSectionSkeleton = () => {
 };
 
 export default async function DashboardPage() {
+  const session = await auth();
+
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
+      <div className="flex items-center justify-between">
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+          Dashboard
+        </h1>
+        <div className="flex gap-x-2 items-center">
+          <UserIcon className="w-5 h-5 -mt-5" />
+          <p className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            {session?.user?.email}
+          </p>
+        </div>
+      </div>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardSectionSkeleton />}>
           <CardWrapper />
